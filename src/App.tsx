972de4d8cdd48d4fc674051a02b84a24f362bad4
Swaroop Bankar <swaroop.devp@gmail.com>
2026-05-13 15:09:41 +0530

    Add Register component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/HomePage/Navbar"
 import Home from "./pages/Home"
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <Register />
     </>
   )
 }
